23e19033f14f0f34752f411d59203b20e8f6705c
Andriei R. S. <andrieiroshild@hotmail.com>
2025-06-27 16:38:48 -0300

    Update 1405.LeetCode.c

--- 1405.LeetCode.c
+++ 1405.LeetCode.c
@@ -1,57 +1,82 @@
+struct Fila {
+    int MAX;
+    int * ini;
+    int * ultimo;
+} fila;
+
+struct Dados {
+    char c;
+    int * prox;
+} dados;
+
 char* longestDiverseString(int a, int b, int c) {
 
-    int * flag = 1;
+    int * flag;
+    *flag = 1;
+    Fila.MAX = 0;
 
     if (a == 0 && b == 0 && c == 0) {
         return "";
     }
 
-    for (flag) {
+    for (; *flag ; ) {
         if (a && a > b && a > c) {
             // insere a
+            //flag = InsereChar();
             // se falhou
             if (!flag) {
                 if (b > c) {
                     // insere b
+                    //flag = InsereChar();
                     // se falhou
                 } else {
                     // insere c
+                    //flag = InsereChar();
                 }
             }
         } else {
             if (b && b > a && b > c) {
                 // insere b
+                //flag = InsereChar();
                 // se falhou
                 if (!flag) {
                     if (a > c) {
                         // insere a
+                        //flag = InsereChar();
                         // se falhou
                     } else {
                         // insere c
+                        //flag = InsereChar();
                     }
                 }
             } else {
                 if (c && c > a && c > b) {
                     // insere c
+                    //flag = InsereChar();
                     // se falhou
                     if (!flag) {
                         if (a > b) {
                             // insere a
+                            //flag = InsereChar();
                             // se falhou
                         } else {
                             //insere b
+                            //flag = InsereChar();
                         }
                     }
-                }
-            } else {
-                if (a > 0 && a == b) {
-                    // insere a
-                }
-                if (a > 0 && a == c) {
-                    // insere a
-                }
-                if (b > 0 && b == c) {
-                    // insere b
+                } else {
+                    if (a > 0 && a == b) {
+                        // insere a
+                        //flag = InsereChar();
+                    }
+                    if (a > 0 && a == c) {
+                        // insere a
+                        //flag = InsereChar();
+                    }
+                    if (b > 0 && b == c) {
+                        // insere b
+                        //flag = InsereChar();
+                    }
                 }
             }
         }
@@ -59,16 +84,35 @@ char* longestDiverseString(int a, int b, int c) {
 
     // string de TAM elementos
     // coloca TAM elementos na string
-    return string
+    //return string;
 }
 
+int IniciaFila () {
+    struct Fila * p;
+    p = (fila *) malloc(sizeof(fila));
+    p->ini = NULL;
+    p->ultimo = NULL;
+}
 
-int Inserechar (int x,int * flag) {
+int Inserechar (char x, int * flag, struct fila * p) {
+    struct Dados * new;
+    new = (dados *) malloc(sizeof(dados));
+    new->c = x;
+
+    // se for o primeiro elemento
+    if (p->ini == NULL) {
+        p->ini = new;
+        p->ultimo = new;
+        new->prox = NULL;
+        p->MAX = 1;
+        return 1;
+    }
+
     // tem espaço ?
-        // Insere o numero
+        // Insere o char
         *flag = 1;
-        return flag;
+        return 1;
     // se não
         *flag = 0;
-        return flag
+        return 0;
 }
